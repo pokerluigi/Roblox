1abe27bef93c9c48a34f9856cf274f878795dc63
EnderChicken1 <111886789+EnderChicken1@users.noreply.github.com>
2022-08-23 18:59:11 +0100

    Update Azure Mines.lua

--- Azure Mines.lua
+++ Azure Mines.lua
@@ -144,7 +144,7 @@ Main:AddToggle({
 	Callback = function(Value)
 		ESPtoggle = Value
 		while ESPtoggle do
-			task.wait(0.2)
+			task.wait(0.666)
 			for _,v in pairs(Mine:GetChildren()) do
 				if v.Name == Ore and not v:FindFirstChildWhichIsA("BillboardGui") then
 					addUi(v)

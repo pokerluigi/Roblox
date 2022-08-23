d1684f64be7385283a43845455d70377c2bafc12
EnderChicken1 <111886789+EnderChicken1@users.noreply.github.com>
2022-08-23 23:30:47 +0100

    Update Azure Mines.lua

--- Azure Mines.lua
+++ Azure Mines.lua
@@ -149,7 +149,7 @@ Main:AddToggle({
 			for _,v in pairs(Mine:GetChildren()) do
 				if v.Name == Ore and not v:FindFirstChildWhichIsA("BillboardGui") then
 					addUi(v)
-					task.wait(0.166)
+					task.wait(0.05)
 				end
 			end
 		end
